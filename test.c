5582c9914086c6e5e175edfe7ef23ef3a63f44a3
predshrim <jeneric1022@gmail.com>
2025-09-13 18:01:20 +0800

    adding sample test data, Modifing code

--- test.c
+++ test.c
@@ -6,18 +6,17 @@ int main()
     int a;
     scanf("%d",&a);
     
-    int record=0;
-    int pn=1;
-    while(a!=0)
+    int record = 0;
+    int pn = 1;
+    while(a)
     {
-        if(pn==1)
-            record+=a%10;
+        if(pn)
+            record += a % 10;
         else
-            record-=a%10;
-        pn*=-1;
-        a=a/10;
+            record -= a % 10;
+        pn = !pn;
+        a /= 10;
     }
 
     printf("%d",abs(record));
-
 }
